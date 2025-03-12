dece9d64c9a6ffb19d1d4396a8bb58c08a8a705f
Anant Sethi <133277891+anantsethi11@users.noreply.github.com>
2025-03-12 10:51:42 +0000

    12/03/25

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -1,10 +1,10 @@
 let score = 33;
 
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
+// console.log(typeof valueInNumber);
 
 //"33" => 33
 //"33abc" => NaN
@@ -13,7 +13,34 @@ console.log(typeof valueInNumber);
 let isLoggedIn = 1
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 let stringNumber = String(Number)
+
+/////***************Not Good way of writing code******************************* */
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+let str1 = "hello"
+let str2 = "hello"
+
+let str3 = str1 + str2
+console.log(str3);
+/* "1" + 2 = 12
+1 + "2" + 2 = 122
+1 + 2 + "2" = 32 */
+
+/* +true =>1
+true+ => error
++"" => 0
+ */
+let num1, num2,num3
+num1 = num2 = num3 = 2+2
+
+let gameCouter = 100
+gameCounter++
+console.log(gameCounter); // 101
+++gameCounter // 101
+ 
 
---
+++ 01_basics/04_comparsions.js
@@ -0,0 +1,9 @@
+/* > < = != == >= <= */
+console.log("2" > 1); // true
+console.log("02" > 1); // true
+
+/* null>0 false
+null ==0 false 
+null >=0 true*/
+
+//===
